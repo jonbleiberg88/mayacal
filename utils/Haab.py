68acb11432d19c84d714193b4b27aac7d02132a8
jonbleiberg88 <jlbleiberg@gmail.com>
2019-11-02 14:18:04 -0400

    adding day daelta support for cal round

--- utils/Haab.py
+++ utils/Haab.py
@@ -3,10 +3,32 @@ haab_months = ["Pop", "Wo", "Sip", "Sotz", "Sek", "Xul", "Yaxkin", "Mol", "Chen"
                 "Kumku", "Wayeb"]
 
 haab_idx_to_month = {idx:month for idx, month in enumerate(haab_months)}
-haab_month_to_idx = {month:idx for idx, month in haab_idx_to_month.keys()}
+haab_month_to_idx = {month:idx for idx, month in haab_idx_to_month.items()}
 
 
 class Haab:
-    def __init__(self, month_name="Kumku", month_number=4):
+    def __init__(self, month_number=4, month_name="Kumku"):
+        if month_name not in haab_months:
+            raise ValueError(f"Invalid Haab month name {month_name}")
         self.month_name = month_name
+
+        if month_name == "Wayeb":
+             if month_number not in list(range(5)):
+                 raise ValueError("Invalid Haab month number, Wayeb number must be between 0 and 4")
+        elif month_number not in list(range(20)):
+            raise ValueError("Invalid Haab month number, must be an integer between 0 and 19")
+
         self.month_number = month_number
+
+        self.haab_num = 20 * haab_month_to_idx[month_name] + month_number
+
+    def reset_by_haab_num(self, new_num):
+        self.month_name = haab_idx_to_month[new_num // 20]
+        self.month_number = new_num % 20
+
+    def add_days(self, num_days):
+        self.haab_num = (self.haab_num + num_days) % 365
+        self.reset_by_haab_num(self.haab_num)
+
+    def __repr__(self):
+        return f"{self.month_number} {self.month_name}"
--- utils/Tzolkin.py
+++ utils/Tzolkin.py
@@ -4,12 +4,41 @@ tzolkin_days = ["Imix", "Ik", "Akbal", "Kan", "Chikchan", "Kimi", "Manik",
                     "Men", "Kib", "Kaban", "Etznab", "Kawak", "Ajaw"]
 
 tzolkin_idx_to_day = {idx:day for idx, day in enumerate(tzolkin_days)}
-tzolkin_day_to_idx = {day:idx for idx, day in tzolkin_day_to_idx.keys()}
+tzolkin_day_to_idx = {day:idx for idx, day in tzolkin_idx_to_day.items()}
+
+tzolkin_num_to_day = {}
+for i in range(260):
+    date = ((i % 13) + 1, tzolkin_idx_to_day[(i % 20)])
+    tzolkin_num_to_day[i] = date
+
+tzolkin_day_to_num = {date:num for num, date in tzolkin_num_to_day.items()}
 
 class Tzolkin:
-    def __init__(self, day_name="Ajaw", day_number=4):
-        self.day_name = day_name
-        self.day_number = day_number
+    def __init__(self, day_number=None, day_name=None, tzolkin_num=None):
+        if tzolkin_num is not None:
+            self.reset_by_tzolkin_num(self, tzolkin_num)
 
-    def add_days(self, days):
-        return
+        else:
+            if day_name not in tzolkin_days:
+                raise ValueError(f"Invalid tzolkin day name {day_name}")
+            self.day_name = day_name
+
+            if day_number not in list(range(1, 13)):
+                raise ValueError("Invalid tzolkin day number - must be integer between 1 and 13")
+            self.day_number = day_number
+
+            self.tzolkin_num = tzolkin_day_to_num[(day_number, day_name)]
+
+    def reset_by_tzolkin_num(self, new_num):
+        if new_num >= 260:
+            raise ValueError("Invalid Tzolkin number, must be between 0 and 259")
+
+        self.tzolkin_num = new_num
+        self.day_number, self.day_name = tzolkin_num_to_day[self.tzolkin_num]
+
+    def add_days(self, num_days):
+        new_num = (self.tzolkin_num + num_days) % 260
+        self.reset_by_tzolkin_num(new_num)
+
+    def __repr__(self):
+        return f"{self.day_number} {self.day_name}"
